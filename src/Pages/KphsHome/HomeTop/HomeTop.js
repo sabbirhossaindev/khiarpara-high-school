269468584d894f09f46718283c8f9c5ba408410c
sabbir hossain <mdsabbir477470@gmail.com>
2022-12-08 15:31:13 +0600

    router set up

---
+++ src/Pages/KphsHome/HomeTop/HomeTop.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './HomeTop.css';
+
+const HomeTop = () => {
+    return (
+        <div>
+            <h2>HomeTop</h2>
+        </div>
+    );
+};
+
+export default HomeTop;
---
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Button, Col, Container, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const NotFound = () => {
+    return (
+        <div className='mt-5 my-5 text-center p-5'>
+            <Container>
+            <Row>
+                <Col md="6">
+                    <div>
+                        <img src='' alt="not found" className='img-fluid rounded' data-aos="flip-left"/>
+                    </div>
+                </Col>
+                <Col md="6">
+                    <div className='mt-5'><h3>Sorry,</h3></div>
+                    <div className='mt-5 my-5'>
+                        <h2>Page Not Found</h2>
+                        <h4 className='mt-2'>The Page you requested could not be found.</h4>
+                        <div className='mt-4'>
+                            <Link to='/'><Button variant='primary'>Go To Home</Button></Link>
+                        </div>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
+        </div>
+    );
+};
+
+export default NotFound;
